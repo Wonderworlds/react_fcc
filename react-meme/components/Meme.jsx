d98cc4cd2371360de7c45170fa8a36871d4fdec8
Floran Mauguin <fmauguin.pro@gmail.com>
2023-09-30 20:49:30 +0200

    meme generator working need API connection

--- react-meme/components/Meme.jsx
+++ react-meme/components/Meme.jsx
@@ -2,22 +2,41 @@ import React from "react";
 import data from '../memesData.js'
 
 export default function Meme() {
-	let url_img
-	function handleOnClick() {
+	const [meme, setMeme] = React.useState({
+		topText: "",
+		bottomText: "",
+		randomImage: data.data.memes[0].url
+		}
+		)
+
+	function getNewMemeImg() {
 		const memesArray = data.data.memes
 		const randomNumber = Math.floor(Math.random() * memesArray.length)
-		url_img = memesArray[randomNumber].url
-		console.log(url_img)
+
+		setMeme(prev => {return {...prev,
+		randomImage: data.data.memes[randomNumber].url
+		}})
+	}
+	function handleChange(event) {
+			const {name, value} = event.target
+			setMeme(prev => {return {
+				...prev,
+				[name]: value
+			}})
 	}
 
 	return (
 		<main>
 			<div className="meme">
-				<input type="text" placeholder="Top Text"/>
-				<input type="text" placeholder="Bottom Text"/>
-				<button onClick={handleOnClick}>Get a new meme image 🖼</button>
+				<input type="text" placeholder="Top Text" name="topText" value={meme.topText} onChange={handleChange}/>
+				<input type="text" placeholder="Bottom Text" name="bottomText" value={meme.bottomText} onChange={handleChange}/>
+				<button onClick={getNewMemeImg}>Get a new meme image 🖼</button>
 			</div>
-			{url_img && <img src={url_img}/>}
+			<div className="meme-rendu">
+				<img src={meme.randomImage} className="meme--img"></img>
+				<h2 className="meme--text top">{meme.topText}</h2>
+				<h2 className="meme--text bottom">{meme.bottomText}</h2>
+			</div>
 		</main>
 	)
 }
